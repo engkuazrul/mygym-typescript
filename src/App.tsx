6a75ce22872dfac5b60ace138258c03134982056
engkuazrul <engku@dormeo.my>
2024-12-03 19:50:11 +0800

    setup tailwind and base page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import Navbar from '@/scenes/navbar';
 
-export function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="app bg-gray-20">
+      <Navbar />
+    </div>
   );
 }
 
-export function WrappedApp() {
-  return (
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
-}
+export default App;
---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,15 @@
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+type Props = {
+  page: string;
+};
+
+function Link({ page }: Props) {
+  return (
+    <AnchorLink className={} href={} onClick={}>
+      {page}
+    </AnchorLink>
+  );
+}
+
+export default Link;
